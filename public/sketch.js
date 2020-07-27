20de4912c96b17c7d9ed6140cf815cf2d8204549
Viktor Ferenczi <ferencziviktor@gmail.com>
2020-07-27 15:06:08 +0200

    refactoring

--- public/sketch.js
+++ public/sketch.js
@@ -1,86 +1,84 @@
 var socket;
-const clearButton = document.getElementById('clear-board');
-const startButton = document.getElementById('getWord');
-const greenButton = document.getElementById('green');
-const blueButton = document.getElementById('blue');
-const yellowButton = document.getElementById('yellow');
-const purpleButton = document.getElementById('purple');
-const redButton = document.getElementById('red');
-const blackButton = document.getElementById('black');
+const clearButton = document.getElementById("clear-board");
+const startButton = document.getElementById("getWord");
+const greenButton = document.getElementById("green");
+const blueButton = document.getElementById("blue");
+const yellowButton = document.getElementById("yellow");
+const purpleButton = document.getElementById("purple");
+const redButton = document.getElementById("red");
+const blackButton = document.getElementById("black");
 
 let stateClient = 0;
 let color = "black";
-startButton.addEventListener('click', start)
+startButton.addEventListener("click", start);
 
-startButton.style.display = 'none';
+startButton.style.display = "none";
 
-function start(){
-  socket.emit('start');
+function start() {
+  socket.emit("start");
   socketOnStart();
 }
 
 function setup() {
-  createCanvas(1910,700);
+  createCanvas(1910, 700);
 
   background("hsl(0, 0%, 100%)");
 
-  socket = io.connect('/');
-  socket.on('mouse', newDrawing)
-  socket.on('board', boardClear)
-  socket.on('online', playerShow)
-  socket.on('start', socketOnStart)
-  socket.on('init', (state)=>{
+  socket = io.connect("/");
+  socket.on("mouse", newDrawing);
+  socket.on("board", boardClear);
+  socket.on("online", playerShow);
+  socket.on("start", socketOnStart);
+  socket.on("init", (state) => {
     stateClient = state;
-    document.getElementById("timer").innerText = "000"
-  })
+    document.getElementById("timer").innerText = "000";
+  });
 }
-function playerShow(player){
-  playerText = document.getElementById("players-online")
-  playerText.innerText = "Wiews: " + player.countp
+function playerShow(player) {
+  playerText = document.getElementById("players-online");
+  playerText.innerText = "Views: " + player.countp;
 }
 
-
-function newDrawing(data){
+function newDrawing(data) {
   stroke("black");
   strokeWeight(4);
-  line(data.x, data.y, data.x2, data.y2)
+  line(data.x, data.y, data.x2, data.y2);
 }
 
-function mouseDragged(){
-    console.log('Sending: ' + mouseX + ', ' + mouseY);
-    var data = {
-        x: mouseX,
-        y: mouseY,
-        x2: pmouseX,
-        y2: pmouseY
-    }
-    socket.emit('mouse', data);
-    stroke(color);
-    if (mouseIsPressed === true){
-      strokeWeight(4);
-      line(mouseX, mouseY, pmouseX, pmouseY)
-    }
-}
-
-clearButton.addEventListener('click', sendBoardSize)
-clearButton.addEventListener('click', start)
-
-function sendBoardSize(){
-  var canvasSize = {
-    width : canvas.width,
-    height : canvas.height
+function mouseDragged() {
+  console.log("Sending: " + mouseX + ", " + mouseY);
+  var data = {
+    x: mouseX,
+    y: mouseY,
+    x2: pmouseX,
+    y2: pmouseY,
+  };
+  socket.emit("mouse", data);
+  stroke(color);
+  if (mouseIsPressed === true) {
+    strokeWeight(4);
+    line(mouseX, mouseY, pmouseX, pmouseY);
   }
-
-  socket.emit('board', canvasSize)
-  boardClear(canvasSize)
 }
 
-function socketOnStart(){
+clearButton.addEventListener("click", sendBoardSize);
+clearButton.addEventListener("click", start);
+
+function sendBoardSize() {
+  var canvasSize = {
+    width: canvas.width,
+    height: canvas.height,
+  };
+
+  socket.emit("board", canvasSize);
+  boardClear(canvasSize);
+}
+
+function socketOnStart() {
   doIt(stateClient);
 }
 
-function boardClear(canvasSize){
-  console.log("kex")
+function boardClear(canvasSize) {
   var ctx = canvas.getContext("2d");
 
   ctx.beginPath();
@@ -90,43 +88,48 @@ function boardClear(canvasSize){
 }
 
 function doIt(seconds) {
-  clearButton.removeEventListener('click',start)
-  clearButton.removeEventListener('click',sendBoardSize)
+  clearButton.removeEventListener("click", start);
+  clearButton.removeEventListener("click", sendBoardSize);
   document.getElementById("timer").innerText = seconds;
   if (seconds > 0) {
-    handle = setTimeout(function() {
+    handle = setTimeout(function () {
       doIt(seconds - 1);
     }, 1000);
   } else {
-      document.getElementById("timer").innerText = "";
-      document.getElementById('guessword').innerText =  ''
-      clearButton.addEventListener('click', sendBoardSize)
-      clearButton.addEventListener('click', start)
+    document.getElementById("timer").innerText = "";
+    document.getElementById("guessword").innerText = "";
+    clearButton.addEventListener("click", sendBoardSize);
+    clearButton.addEventListener("click", start);
   }
 }
 
-greenButton.addEventListener('click', greenShift)
-redButton.addEventListener('click', redShift)
-yellowButton.addEventListener('click',yellowShift)
-purpleButton.addEventListener('click', purpleShift)
-blueButton.addEventListener('click', blueShift)
-blackButton.addEventListener('click', blackShift)
+greenButton.addEventListener("click", greenShift);
+redButton.addEventListener("click", redShift);
+yellowButton.addEventListener("click", yellowShift);
+purpleButton.addEventListener("click", purpleShift);
+blueButton.addEventListener("click", blueShift);
+blackButton.addEventListener("click", blackShift);
 
-function redShift(){
+function redShift() {
   color = "red";
 }
-function greenShift(){
+
+function greenShift() {
   color = "green";
 }
-function yellowShift(){
+
+function yellowShift() {
   color = "yellow";
 }
-function purpleShift(){
+
+function purpleShift() {
   color = "purple";
 }
-function blueShift(){
+
+function blueShift() {
   color = "blue";
 }
-function blackShift(){
+
+function blackShift() {
   color = "black";
 }
--- server.js
+++ server.js
@@ -1,50 +1,48 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 let state = 100;
 let player = {
-    countp: 0
+  countp: 0,
 };
 
-
 const port = process.env.PORT || 3000;
-var server = app.listen(port, () =>{
-    console.log('Megy a szerver itten :' + port)    
+var server = app.listen(port, () => {
+  console.log("The server is running :" + port);
 });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-var socket = require('socket.io');
+var socket = require("socket.io");
 
 var io = socket(server);
 
-io.sockets.on('connection', newConnection);
-
+io.sockets.on("connection", newConnection);
 
 function newConnection(socket) {
-    console.log("uj konnekcio: " + socket.id);
-    socket.emit('init', state);
-    socket.on('mouse', mouseMsg);
-    socket.on('board', clearBoard);
-    socket.on('start', onStart);
-    socket.broadcast.emit('online', player);
-    socket.emit('online', player);
-    console.log("osszes player: "+ player.countp)
-    player.countp = player.countp + 1;
+  console.log("New connection: " + socket.id);
+  socket.emit("init", state);
+  socket.on("mouse", mouseMsg);
+  socket.on("board", clearBoard);
+  socket.on("start", onStart);
+  socket.broadcast.emit("online", player);
+  socket.emit("online", player);
+  console.log("Player count: " + player.countp);
+  player.countp = player.countp + 1;
 
-    function onStart() {
-        state = 100;
-        socket.broadcast.emit('start', state)
-    }
+  function onStart() {
+    state = 100;
+    socket.broadcast.emit("start", state);
+  }
 
-    function clearBoard(canvasSize){
-        socket.broadcast.emit('board',canvasSize);
-        console.log("sikerult elkuldeni a meretet: " + canvasSize.width);
-    }
+  function clearBoard(canvasSize) {
+    socket.broadcast.emit("board", canvasSize);
+    console.log("The sent size: " + canvasSize.width);
+  }
 
-    function mouseMsg(data){
-        socket.broadcast.emit('mouse',data);
-        console.log(data);
-    }
+  function mouseMsg(data) {
+    socket.broadcast.emit("mouse", data);
+    console.log(data);
+  }
 }
 
-app.get('/go',(res,req) => console.log('xd'))
+app.get("/go", (res, req) => console.log("go"));
